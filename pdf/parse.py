64b5c71a3b241b0b7818ef71dcdf8cef01acaee9
obelisk2u <jdstout@gmail.com>
2025-01-02 22:18:37 +1300

    again jus tryna be a billionaire

--- pdf/parse.py
+++ pdf/parse.py
@@ -1,8 +1,25 @@
-import openparse
+import PyPDF2
+from openai import OpenAI
+import os
 from pdf2image import convert_from_path
 from pytesseract import image_to_string
 from PIL import Image
-import PyPDF2
+
+client = OpenAI()
+
+def sendtogpt(text):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",  
+        messages=[
+            {"role": "system", "content": "You will respond with only latex code with the answers. The answers should be numbered based on which question they correspond to. When writing LaTeX code, ensure that the expressions are properly formatted using appropriate math commands. Remove unnecessary escape sequences and redundant expressions, and ensure that each formula is clearly written in standard LaTeX syntax."},
+            {"role": "user", "content": text}
+        ]
+    )
+    message = response.choices[0].message
+    print(message)
+    #with open("response.txt", "w", encoding="utf-8") as file:
+    #    file.write(message)
+
 
 def regular():
 
@@ -13,7 +30,8 @@ def regular():
         for page in reader.pages:
             text += page.extract_text()
         print(text)
-        
+
+    sendtogpt(text)   
     with open("output.txt", "w", encoding="utf-8") as file:
         file.write(text)
 
